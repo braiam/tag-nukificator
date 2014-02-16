ea56c3aa4af9c77c0f98ee7b70680441c08a791b
Undo1 <parker@erwaysoftware.com>
2014-02-15 18:13:04 -0700

    move that

--- nukificator.php
+++ nukificator.php
@@ -1,14 +1,14 @@
 <?php
 include "config.php";
 
+echo 'Tag to inspect: ';
+
+$handle = fopen ("php://stdin","r");
+$line = fgets($handle);
+
+$tag = trim($line);
+
 while(1)
 {
-	echo 'Tag to inspect: ';
 
-	$handle = fopen ("php://stdin","r");
-	$line = fgets($handle);
-
-	$tag = trim($line);
-
-	
 }
