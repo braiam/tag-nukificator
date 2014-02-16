6c4213a8ba9422b3a0a59a33465be2144e7ec16f
Undo1 <parker@erwaysoftware.com>
2014-02-15 19:57:31 -0700

    All that's left is the actual removal...

--- nukificator.php
+++ nukificator.php
@@ -8,9 +8,38 @@ $line = fgets($handle);
 
 $tag = trim($line);
 
-while(1)
-{
+echo "fetching last 100 [" . $tag  . "] questions...";
 
+$commentinfourl = 'https://api.stackexchange.com/2.2/questions';
+$commentinfodata = array("site" => 'stackoverflow', "tagged" => $tag, "key" => "6Z09liTt4uTQU*a4DYOXVQ((", "access_token" => StackAccessToken(), "filter" => "!)Q3J5qpRx7SYX9_raoN3Q4cY");
+$response = (new Curl)->exec($commentinfourl . '?' . http_build_query($commentinfodata), [CURLOPT_ENCODING => 'gzip']);
+
+echo " fetched" . PHP_EOL;
+
+$obj=json_decode($response);
+
+$questions = $obj->{"items"};
+
+$colors = new Colors();
+
+foreach ($questions as $question)
+{
+	echo PHP_EOL . "    " . $colors->getColoredString($question->{"title"}, "red") . PHP_EOL . PHP_EOL;
+
+	echo $colors->getColoredString(mb_substr($question -> {"body_markdown"}, 0, 500), "blue") . PHP_EOL . PHP_EOL;
+
+	foreach ($question->{"tags"} as $qtag) {
+		echo $colors->getColoredString("[" . $qtag . "] ", "white", "red");
+	}
+
+	echo PHP_EOL . PHP_EOL;
+
+	echo "rm [" . $tag . "] tag? (y/n): ";
+
+	$handle = fopen ("php://stdin","r");
+	$line = fgets($handle);
+
+	$response = trim($line);
 }
 
 class Curl
@@ -48,4 +77,67 @@ class Curl
   {
     return $this->info;
   }
+}
+class Colors {
+
+	private $foreground_colors = array();
+	private $background_colors = array();
+
+	public function __construct() {
+		// Set up shell colors
+		$this->foreground_colors['black'] = '0;30';
+		$this->foreground_colors['dark_gray'] = '1;30';
+		$this->foreground_colors['blue'] = '0;34';
+		$this->foreground_colors['light_blue'] = '1;34';
+		$this->foreground_colors['green'] = '0;32';
+		$this->foreground_colors['light_green'] = '1;32';
+		$this->foreground_colors['cyan'] = '0;36';
+		$this->foreground_colors['light_cyan'] = '1;36';
+		$this->foreground_colors['red'] = '0;31';
+		$this->foreground_colors['light_red'] = '1;31';
+		$this->foreground_colors['purple'] = '0;35';
+		$this->foreground_colors['light_purple'] = '1;35';
+		$this->foreground_colors['brown'] = '0;33';
+		$this->foreground_colors['yellow'] = '1;33';
+		$this->foreground_colors['light_gray'] = '0;37';
+		$this->foreground_colors['white'] = '1;37';
+
+		$this->background_colors['black'] = '40';
+		$this->background_colors['red'] = '41';
+		$this->background_colors['green'] = '42';
+		$this->background_colors['yellow'] = '43';
+		$this->background_colors['blue'] = '44';
+		$this->background_colors['magenta'] = '45';
+		$this->background_colors['cyan'] = '46';
+		$this->background_colors['light_gray'] = '47';
+	}
+
+	// Returns colored string
+	public function getColoredString($string, $foreground_color = null, $background_color = null) {
+		$colored_string = "";
+
+		// Check if given foreground color found
+		if (isset($this->foreground_colors[$foreground_color])) {
+			$colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+		}
+		// Check if given background color found
+		if (isset($this->background_colors[$background_color])) {
+			$colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+		}
+
+		// Add string and end coloring
+		$colored_string .=  $string . "\033[0m";
+
+		return $colored_string;
+	}
+
+	// Returns all foreground color names
+	public function getForegroundColors() {
+		return array_keys($this->foreground_colors);
+	}
+
+	// Returns all background color names
+	public function getBackgroundColors() {
+		return array_keys($this->background_colors);
+	}
 }
